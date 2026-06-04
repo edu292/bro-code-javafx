657f276cf1aad989c744f2b8709655d73e5d6942
edu292 <eduskoroboatei@gmail.com>
2026-06-04 15:33:00 -0300

    Communication between controllers

---
+++ communication-controllers/src/main/java/com/edusk/Scene1Controller.java
@@ -0,0 +1,33 @@
+package com.edusk;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Scene1Controller {
+    @FXML
+    TextField nameField;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void login(ActionEvent event) throws IOException {
+        String username = nameField.getText();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("scene2.fxml"));
+        root = loader.load();
+        Scene2Controller scene2Controller = loader.getController();
+        scene2Controller.displayName(username);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
