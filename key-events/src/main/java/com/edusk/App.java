e62df17e1f82ca519ee48697908a37e5e26b5104
edu292 <eduskoroboatei@gmail.com>
2026-06-04 18:32:38 -0300

    key-events

---
+++ key-events/src/main/java/com/edusk/App.java
@@ -0,0 +1,48 @@
+package com.edusk;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("app.fxml"));
+        Parent root = loader.load();
+        scene = new Scene(root, 640, 480);
+        Controller controller = loader.getController();
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case W -> controller.moveUp();
+                case S -> controller.moveDown();
+                case A -> controller.moveLeft();
+                case D -> controller.moveRight();
+                default -> System.out.println(event.getCode());
+            }
+        });
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ key-events/src/main/java/com/edusk/Controller.java
@@ -0,0 +1,20 @@
+package com.edusk;
+
+public class Controller {
+    public void moveUp() {
+        System.out.println("Movin' Up!");
+    }
+
+    public void moveDown() {
+        System.out.println("Movin' Down!");
+    }
+
+    public void moveLeft() {
+        System.out.println("Movin' Left!");
+    }
+
+    public void moveRight() {
+        System.out.println("Movin' Right!");
+    }
+
+}
